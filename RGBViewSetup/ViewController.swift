ac2a75e1f95c37a7fded27026648368baa5d6ee8
Alexey <alexeyradomskiy98@gmail.com>
2022-03-08 14:36:42 +0300

    Initial Commit

---
+++ RGBViewSetup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBViewSetup
+//
+//  Created by Alexey on 08.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
